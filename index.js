7c1e82ccf42e851d6caa582b9e480a4105e1ec31
ryanfalco13@gmail.com <ryanfalco13@gmail.com>
2023-06-27 13:57:07 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Remove any spaces and convert the word to lowercase
+  word = word.replace(/\s/g, "").toLowerCase();
+
+  // Initialize two pointers, one at the start and one at the end of the word
+  let left = 0;
+  let right = word.length - 1;
+
+  // Iterate until the two pointers meet or cross each other
+  while (left < right) {
+    // If the characters at the left and right pointers are not the same, the word is not a palindrome
+    if (word[left] !== word[right]) {
+      return false;
+    }
+
+    // Move the pointers towards each other
+    left++;
+    right--;
+  }
+
+  // If the loop completes without returning false, the word is a palindrome
+  return true;
 }
+
+module.exports = isPalindrome;
+
 
 /* 
   Add your pseudocode here
+  1. Remove any spaces from the word and convert it to lowercase.
+2. Initialize two pointers, `left` and `right`, with `left` starting from the beginning of the word and `right` starting from the end of the word.
+3. Repeat the following steps while `left` is less than `right`:
+     a. If the characters at `left` and `right` pointers are not the same, return false as the word is not a palindrome.
+     b. Move `left` one step forward and `right` one step backward.
+4. If the loop completes without returning false, return true as the word is a palindrome.
+
 */
 
 /*
